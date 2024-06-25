34b45d1afff2a3606d604c47ac1040c2d81a8391
Sam Bull <git@sambull.org>
2024-06-25 18:47:26 +0100

    Updates for flights

--- otds/enums.py
+++ otds/enums.py
@@ -8,8 +8,62 @@ class Absolute(Enum):
     DayBase = "DayBase"
     PersonBase = "PersonBase"
 
+class Percent(Enum):
+    ApplyTo = "ApplyTo"
+
+class AccommodationTargetgroup(Enum):
+    GourmetTravellers = "GourmetTravellers"
+    Singles = "Singles"
+    LuxuryTravellers = "LuxuryTravellers"
+    Families = "Families"
+    SeniorTravellers = "SeniorTravellers"
+    RomanticCouples = "RomanticCouples"
+    YoungPeople = "YoungPeople"
+    AdultsOnly = "AdultsOnly"
+    Homosexuals = "Homosexuals"
+    AccomodationForCityTravellers = "AccomodationForCityTravellers"
+    AccomodationForCulturalTravellers = "AccomodationForCulturalTravellers"
+    AccomodationForNatureTravellers = "AccomodationForNatureTravellers"
+    WellnessTravellers = "WellnessTravellers"
+    SportTravellers = "SportTravellers"
+    ShortstayTravellers = "ShortstayTravellers"
+    DogOwner = "DogOwner"
+    HoneyMooner = "HoneyMooner"
+    Backpacker = "Backpacker"
+    BusinessTravellers = "BusinessTravellers"
+    ClubTourists = "ClubTourists"
+    CruiseTravellers = "CruiseTravellers"
+    GolfTourists = "GolfTourists"
+    GroupTourists = "GroupTourists"
+    LongTermTourists = "LongTermTourists"
+    Cyclists = "Cyclists"
+    Motorbiker = "Motorbiker"
+    RoundtripTravellers = "RoundtripTravellers"
+    SingleWithChild = "SingleWithChild"
+    WheelchairUsers = "WheelchairUsers"
+    BeachTravellers = "BeachTravellers"
+    PartyTourists = "PartyTourists"
+
 class AccommodationType(Enum):
     Hotel = "Hotel"
+    Club = "Club"
+    ApartmentHotel = "ApartmentHotel"
+    HolidayPark = "HolidayPark"
+    Villa = "Villa"
+    HolidayFlat = "HolidayFlat"
+    CaravanSite = "CaravanSite"
+    Other = "Other"
+    BungalowResort = "BungalowResort"
+    Family = "Family"
+    DetachedHouse = "DetachedHouse"
+    RowHouse = "RowHouse"
+    HolidayHome = "HolidayHome"
+    ElegantAccommodationStyle = "ElegantAccommodationStyle"
+    BoutiqueAccommodationStyle = "BoutiqueAccommodationStyle"
+    DesignAccommodationStyle = "DesignAccommodationStyle"
+    HistoricAccommodationStyle = "HistoricAccommodationStyle"
+    RuralAccommodationStyle = "RuralAccommodationStyle"
+    PalaceHotelStyle = "PalaceHotelStyle"
 
 class AgeType(Enum):
     DateOfBirth = "DateOfBirth"
@@ -20,6 +74,17 @@ class AirportType(Enum):
     Arrival = "Arrival"
     Catchment = "Catchment"
     Departure = "Departure"
+
+class AvailabilityState(Enum):
+    Open = "Open"
+    Closed = "Closed"
+    Request = "Request"
+    StopSales = "StopSales"
+    Blacklisted = "Blacklisted"
+
+class BaggageType(Enum):
+    Cabin = "Cabin"
+    Checked = "Checked"
 
 class BoardType(Enum):
     none = "None"
@@ -53,6 +118,8 @@ class BookingParameter(Enum):
 
 class CombinableWhen(Enum):
     Code = "CombinationCode"
+    IndexMin = "IndexMin"
+    Not = "Not"
     Or = "Or"
 
 class Component(Enum):
@@ -61,11 +128,20 @@ class Component(Enum):
     DefinedComponent = "DefinedComponent"
     OnewayFlight = "OnewayFlight"
 
+class ComponentAttribute(Enum):
+    ThisComponent = "ThisComponent"
+    Accommodation = "Accommodation"
+    OnewayFlight = "OnewayFlight"
+    ReturnFlight = "ReturnFlight"
+    Flight = "Flight"
+    Addon = "Addon"
+
 class Condition(Enum):
     And = "And"
     Or = "Or"
     Not = "Not"
     Airports = "Airports"
+    BookingDate = "BookingDate"
     BookingDateOffset = "BookingDateOffset"
     ConditionalTags = "ConditionalTags"
     Date = "Date"
@@ -76,8 +152,10 @@ class Condition(Enum):
     Keys = "Keys"
     MatchEqual = "MatchEqual"
     PersonCount = "PersonCount"
+    PersonGroup = "PersonGroup"
     PersonImpact = "PersonImpact"
     Tags = "Tags"
+    Weekdays = "Weekdays"
 
 class CrsSystem(Enum):
     Merlin = "Merlin"
@@ -102,8 +180,10 @@ class DayAllocationPart(Enum):
 class DayImpact(Enum):
     Date = "Date"
     DayIndex = "DayIndex"
+    Weekdays = "Weekdays"
 
 class DayIndex(Enum):
+    From = "From"
     Indices = "Indices"
     Until = "Until"
 
@@ -119,10 +199,12 @@ class DayType(Enum):
 class DayState(Enum):
     Closed = "Closed"
     Open = "Open"
+    Request = "Request"
 
 class DefaultDayState(Enum):
     Closed = "Closed"
     Open = "Open"
+    Request = "Request"
 
 class DistributionChannel(Enum):
     Bewotec = "Bewotec"
@@ -142,6 +224,10 @@ class EvaluationBase(Enum):
     PersonDay = "Person Day"
     Person = "Person"
     Day = "Day"
+
+class EvaluationMode(Enum):
+    Any = "Any"
+    All = "All"
 
 class Field(Enum):
     Title = "Title"
@@ -187,6 +273,7 @@ class GeneralIncludedService(Enum):
 
 class Match(Enum):
     Element = "Element"
+    Key = "Key"
     Tag = "Tag"
 
 class MatchElement(Enum):
@@ -195,6 +282,7 @@ class MatchElement(Enum):
     ArrivalAirport = "ArrivalAirport"
 
 class Occupancy(Enum):
+    Exclude = "Exclude"
     Person = "Person"
 
 class OptionalBookableAddonType(Enum):
@@ -230,6 +318,9 @@ class PersonImpact(Enum):
     Age = "Age"
     Genders = "Genders"
     Index = "Index"
+
+class PersonIndexFilter(Enum):
+    Tags = "Tags"
 
 class ProductType(Enum):
     AccommodationOnly = "AccommodationOnly"
@@ -353,3 +444,12 @@ class UpdateMode(Enum):
     New = "New"
     Merge = "Merge"
     Delete = "Delete"
+
+class Weekday(Enum):
+    Mon = "Mon"
+    Tue = "Tue"
+    Wed = "Wed"
+    Thu = "Thu"
+    Fri = "Fri"
+    Sat = "Sat"
+    Sun = "Sun"
--- otds/typedefs.py
+++ otds/typedefs.py
@@ -11,8 +11,12 @@ AgencyCode = NewType("AgencyCode", str)
 AvailabilityOpen = NewType("AvailabilityOpen", int)
 AvailabilityRequest = NewType("AvailabilityRequest", int)
 BrandCode = NewType("BrandCode", str)
+CheckInOutOffset = NewType("CheckInOutOffset", int)
+CheckOutDateOffset = NewType("CheckOutDateOffset", int)
 DayAllocationIndex = NewType("DayAllocationIndex", int)
 DayAllocationLevel = NewType("DayAllocationLevel", int)
+IataAirlineCode = NewType("IataAirlineCode", str)  # 2 char IATA or 3 char ICAO
+IataAirportCode = NewType("IataAirportCode", str)  # [A-Z]{3}
 Identifier = NewType("Identifier", str)  # [A-Za-z0-9\.\-_|\+]+
 ISO3166Country = NewType("ISO3166Country", str)  # [A-Z]{2}
 Key = NewType("Key", str)  # [A-Za-z0-9\.\-_|\+]+
@@ -23,19 +27,32 @@ Name = NewType("Name", str)  # [A-Za-z0-9]+
 Offset = NewType("Offset", int)
 OWGS84Latitude = NewType("OWGS84Latitude", float)  # -90.0 to 90.0
 OWGS84Longitude = NewType("OWGS84Longitude", float)  # -180.0 to 180.0
+PersonAge = NewType("PersonAge", int)
+PriceItemClass = NewType("PriceItemClass", str)  # Same restriction as Token
 SeparatorLeft = NewType("SeparatorLeft", str)
 SeparatorRight = NewType("SeparatorRight", str)
 ShortServiceAnnotation = NewType("ShortServiceAnnotation", str)  # 1-50 characters
 SimpleNodeIataAirportCode = NewType("SimpleNodeIataAirportCode", str)
+Size = NewType("Size", tuple[float, str | None])  # (value, unit)
 SourceAttribute = NewType("SourceAttribute", str)
 StringSlice = NewType("StringSlice", tuple[int, int | None])
 
 _AbsoluteConditionAppliedBy = tuple[Literal[e.Absolute.AppliedBy], str]
 _AbsoluteConditionDayBase = tuple[Literal[e.Absolute.DayBase], tuple[SourceAttribute, int | Literal[e.X.x]]]
-_AbsoluteConditionPersonBase = tuple[Literal[e.Absolute.PersonBase], int]
+_AbsoluteConditionPersonBase = tuple[Literal[e.Absolute.PersonBase], int | Literal[e.X.x]]
 AbsoluteCondition = _AbsoluteConditionAppliedBy | _AbsoluteConditionDayBase | _AbsoluteConditionPersonBase
 
+PercentCondition = tuple[Literal[e.Percent.ApplyTo], tuple[PriceItemClass, ...]]
+
 AccommodationCategory = tuple[Literal[0, 1, 2, 3, 4, 5, 6], Literal[0, 5]] | tuple[Literal[7], Literal[0]]
+
+class AgeCondition(TypedDict, total=False):
+    max: int
+    min: int
+
+class BookingDateCondition(TypedDict, total=False):
+    min: datetime.date
+    max: datetime.date
 
 class BookingOffsetCondition(TypedDict, total=False):
     min: int
@@ -47,31 +64,43 @@ class DateCondition(TypedDict, total=False):
     dates: tuple[datetime.date, ...]
 
 _DayImpactDate = tuple[Literal[e.DayImpact.Date], tuple[e.DayType, SourceAttribute, DateCondition]]
-_DayImpactDayIndex = tuple[Literal[e.DayImpact.DayIndex], tuple[SourceAttribute, tuple[tuple[e.DayIndex, int], ...]]]
-DayImpact = _DayImpactDate | _DayImpactDayIndex
+_DayImpactDayIndex = tuple[Literal[e.DayImpact.DayIndex], tuple[SourceAttribute, tuple[tuple[e.DayIndex, int], ...]], int | None]
+_DayImpactWeekdays = tuple[Literal[e.DayImpact.Weekdays], tuple[SourceAttribute, e.DayType, tuple[e.Weekday, ...]]]
+DayImpact = _DayImpactDate | _DayImpactDayIndex | _DayImpactWeekdays
 
 class DurationCondition(TypedDict, total=False):
     durations: tuple[int, ...]
     min: datetime.timedelta
     max: datetime.timedelta
+    multiples: int
 
 _MatchElement = tuple[Literal[e.Match.Element], tuple[e.MatchElement, SourceAttribute]]
+_MatchKey = tuple[Literal[e.Match.Key], tuple[SourceAttribute]]
 _MatchTag = tuple[Literal[e.Match.Tag], tuple[SourceAttribute, Token]]
-Match = _MatchElement | _MatchTag
+Match = _MatchElement | _MatchKey | _MatchTag
 
-class PersonAge(TypedDict, total=False):
-    max: int
+class OccupancyConditionPerson(TypedDict, total=False):
+    min_age: PersonAge
+    min_count: int
+
+class OccupancyPerson(TypedDict, total=False):
+    count: int
+    max_age: Age
+    min_age: Age
+    max_count: int
+    min_count: int
 
 class PersonCount(TypedDict, total=False):
     filter: tuple[Literal[e.PersonFilter.Impact], tuple[SourceAttribute, Token, tuple[str, ...]]]
     min: int
 
 class PersonIndex(TypedDict, total=False):
+    filter: tuple[tuple[e.PersonIndexFilter, tuple[SourceAttribute, Token, tuple[str, ...]]], ...]
     from_: int
     indices: tuple[int, ...]
     until: int
 
-_PersonImpactAge = tuple[Literal[e.PersonImpact.Age], tuple[SourceAttribute, PersonAge]]
+_PersonImpactAge = tuple[Literal[e.PersonImpact.Age], tuple[SourceAttribute, AgeCondition]]
 _PersonImpactIndex = tuple[Literal[e.PersonImpact.Index], tuple[SourceAttribute, PersonIndex]]
 _PersonImpactGender = tuple[Literal[e.PersonImpact.Genders], tuple[SourceAttribute, tuple[e.PersonGender, ...]]]
 PersonImpact = _PersonImpactAge | _PersonImpactIndex | _PersonImpactGender
@@ -79,6 +108,7 @@ PersonImpact = _PersonImpactAge | _PersonImpactIndex | _PersonImpactGender
 _ConditionAndOr = tuple[Literal[e.Condition.And, e.Condition.Or], tuple["ConditionGroup", ...]]
 _ConditionNot = tuple[Literal[e.Condition.Not], "ConditionGroup"]
 _ConditionAirports = tuple[Literal[e.Condition.Airports], tuple[SourceAttribute, e.AirportType, tuple[str, ...]]]
+_ConditionBookingDate = tuple[Literal[e.Condition.BookingDate], tuple[SourceAttribute, BookingDateCondition]]
 _ConditionBookingDateOffset = tuple[Literal[e.Condition.BookingDateOffset], tuple[SourceAttribute, BookingOffsetCondition]]
 _ConditionConditionalTags = tuple[Literal[e.Condition.ConditionalTags], tuple[SourceAttribute, Token, tuple[str, ...]]]
 _ConditionDate = tuple[Literal[e.Condition.Date], tuple[e.DayType, SourceAttribute, DateCondition]]
@@ -89,9 +119,11 @@ _ConditionImply = tuple[Literal[e.Condition.Imply], tuple["ConditionGroup", "Con
 _ConditionKeys = tuple[Literal[e.Condition.Keys], tuple[SourceAttribute, str, e.DayAllocation | None]]
 _ConditionMatchEqual = tuple[Literal[e.Condition.MatchEqual], tuple[Match, ...]]
 _ConditionPersonCount = tuple[Literal[e.Condition.PersonCount], tuple[SourceAttribute, PersonCount]]
+_ConditionPersonGroup = tuple[Literal[e.Condition.PersonGroup], tuple[SourceAttribute, tuple[OccupancyConditionPerson, ...]]]
 _ConditionPersonImpact = tuple[Literal[e.Condition.PersonImpact], PersonImpact]
-_ConditionTags = tuple[Literal[e.Condition.Tags], tuple[SourceAttribute, Token, tuple[str, ...], StringSlice]]
-ConditionGroup: TypeAlias = _ConditionAndOr | _ConditionNot | _ConditionAirports | _ConditionBookingDateOffset | _ConditionConditionalTags | _ConditionDayImpact | _ConditionDate | _ConditionDuration | _ConditionImpact | _ConditionImply | _ConditionKeys | _ConditionMatchEqual | _ConditionPersonCount | _ConditionPersonImpact | _ConditionTags
+_ConditionTags = tuple[Literal[e.Condition.Tags], tuple[SourceAttribute, Token, tuple[str, ...], StringSlice, e.EvaluationMode, e.DayAllocation]]
+_ConditionWeekdays = tuple[Literal[e.Condition.Weekdays], tuple[SourceAttribute, e.DayType, tuple[e.Weekday, ...]]]
+ConditionGroup: TypeAlias = _ConditionAndOr | _ConditionNot | _ConditionAirports | _ConditionBookingDate | _ConditionBookingDateOffset | _ConditionConditionalTags | _ConditionDayImpact | _ConditionDate | _ConditionDuration | _ConditionImpact | _ConditionImply | _ConditionKeys | _ConditionMatchEqual | _ConditionPersonCount | _ConditionPersonGroup | _ConditionPersonImpact | _ConditionTags | _ConditionWeekdays
 
 _DayStateClosed = tuple[Literal[e.DayState.Closed]]
 _DayStateOpen = tuple[Literal[e.DayState.Open], AvailabilityOpen | None]
@@ -104,7 +136,8 @@ DefaultDayState = _DefaultDayStateClosed | _DefaultDayStateOpen
 class DefaultDayStateExtra(TypedDict, total=False):
     check_out: AvailabilityRequest | None
 
-Availability = tuple[datetime.date, datetime.date, tuple[DefaultDayState, DefaultDayStateExtra], Mapping[Key, tuple[Offset, DayState]]]
+_CheckIn = _CheckOut = e.AvailabilityState | Literal[False] | None
+Availability = tuple[datetime.date, datetime.date, tuple[DefaultDayState, DefaultDayStateExtra], Mapping[Key, tuple[Offset, DayState, _CheckIn, _CheckOut]]]
 Availabilities = tuple[ConditionGroup | None, Mapping[Key, Availability]]
 
 _BookingParameterDate = tuple[Literal[e.BookingParameter.Date], e.DayType, SourceAttribute, e.DateFormat]
@@ -113,9 +146,10 @@ _BookingParameterTag = tuple[Literal[e.BookingParameter.Tag], SourceAttribute, T
 _BookingParameterValue = tuple[Literal[e.BookingParameter.Value], str]
 BookingParameterParam = _BookingParameterDate | _BookingParameterPersonAge | _BookingParameterTag | _BookingParameterValue
 
-_CombinableWhenCode = tuple[Literal[e.CombinableWhen.Code], Identifier]
-_CombinableWhenOr = tuple[Literal[e.CombinableWhen.Or], tuple["CombinableWhen", ...]]
-CombinableWhen = _CombinableWhenCode | _CombinableWhenOr
+_CombinableWhenCode = tuple[Literal[e.CombinableWhen.Code], Identifier, Identifier]
+_CombinableWhenIndexMin = tuple[Literal[e.CombinableWhen.IndexMin], Identifier, int]
+_CombinableWhenCond = tuple[Literal[e.CombinableWhen.Or, e.CombinableWhen.Not], tuple["CombinableWhen", ...]]
+CombinableWhen = _CombinableWhenCode | _CombinableWhenIndexMin | _CombinableWhenCond
 
 class BookingParameter(TypedDict):
     field: e.Field
@@ -127,15 +161,16 @@ class BookingParameter(TypedDict):
 _BookingGroupBookingParameter = tuple[Literal[e.BookingGroup.Parameter], BookingParameter]
 _BookingGroupCondition = tuple[Literal[e.BookingGroup.Condition], ConditionGroup]
 BookingGroupCondition = _BookingGroupBookingParameter | _BookingGroupCondition
-BookingGroup = tuple[e.BookingGroupArea, SourceAttribute, tuple[BookingGroupCondition, ...], e.EvaluationBase | None]
+BookingGroup = tuple[e.BookingGroupArea, SourceAttribute, tuple[BookingGroupCondition, ...], e.EvaluationBase | None, int]
 
 DayAllocationStartEnd = tuple[DayAllocationLevel, SourceAttribute, e.DayReference, e.Shift]
 _DayAllocationStart = tuple[Literal[e.DayAllocationPart.Start], DayAllocationStartEnd]
 _DayAllocationEnd = tuple[Literal[e.DayAllocationPart.End], DayAllocationStartEnd]
 DayAllocation = _DayAllocationStart | _DayAllocationEnd
 
-_OccupancyPerson = tuple[Literal[e.Occupancy.Person], "OccupancyPerson"]
-Occupancy = _OccupancyPerson
+_OccupancyPerson = tuple[Literal[e.Occupancy.Person], OccupancyPerson]
+_OccupancyExclude = tuple[Literal[e.Occupancy.Exclude], tuple[OccupancyPerson, ...]]
+Occupancy = _OccupancyPerson | _OccupancyExclude
 
 OptionalBookableAddonType = tuple[e.OptionalBookableAddonType, ShortServiceAnnotation]
 
@@ -170,6 +205,7 @@ class Geo(TypedDict, total=False):
 class Address(TypedDict, total=False):
     city: LanguageText
     country: LanguageText
+    fax: str
     geo: Geo
     phone: str
     street: str
@@ -179,34 +215,64 @@ class AccommodationInfo(TypedDict, total=False):
     giata: tuple[str, str]
     geonames: tuple[str, str]
 
+class Baggage(TypedDict, total=False):
+    pieces: int
+    weight: Size
+
 class Combinatorics(TypedDict, total=False):
     code: tuple[Identifier, str]
+    index: tuple[Identifier, int]
     level: int
     when: CombinableWhen
 
+class NeighbourComponentCorrection(TypedDict, total=False):
+    check_in_offset: tuple[CheckInOutOffset, e.ComponentAttribute | None]
+    check_out_offset: tuple[CheckInOutOffset, e.ComponentAttribute | None]
+
+class Operating(TypedDict, total=False):
+    carrier: IataAirlineCode
+    flight_number: str
+
 class PriceItem(TypedDict, total=False):
     absolute: tuple[Decimal, tuple[AbsoluteCondition, ...]]
+    percent: tuple[Decimal, tuple[PercentCondition, ...]]
     combinatorics: Mapping[tuple[Identifier, LayerLevel], Combinatorics]
     condition: ConditionGroup
 
+class RouteNode(TypedDict, total=False):
+    airport: IataAirportCode
+    date_offset: int
+    time: datetime.time
+
+class Route(TypedDict, total=False):
+    arrival: RouteNode
+    departure: RouteNode
+    operating: Operating
+    stop_overs: int
+
 class Property(TypedDict, total=False):
     address: Address
+    baggage_allowances: Mapping[e.BaggageType, Baggage]
     board_name: LanguageText
     board_type: e.BoardType
     city: tuple[LanguageText, ...]
     condition: ConditionGroup
+    flight_routes: tuple[Route, ...]
     included_services: tuple[e.GeneralIncludedService, ...]
     info: AccommodationInfo
     name: LanguageText
     official_category: AccommodationCategory
     operator_category: AccommodationCategory
     optional_addons: tuple[OptionalBookableAddonType, ...]
+    target_groups: tuple[e.AccommodationTargetgroup, ...]
+    type: e.AccommodationType
     unit_facilities: tuple[e.UnitFacilities, ...]
     unit_name: LanguageText
     unit_types: tuple[e.UnitType, ...]
 
 class Board(TypedDict, total=False):
     booking: tuple[BookingGroup, ...]
+    price_items: Mapping[Key, Mapping[Token, tuple[PriceItem, ...]]]
     properties: Mapping[Key, tuple[Property, ...]]
     tags: TagsDict
 
@@ -237,6 +303,11 @@ class Accommodation(TypedDict, total=False):
 
 class BookingClass(TypedDict, total=False):
     availabilities: Mapping[Key, Availabilities]
+    booking: tuple[BookingGroup, ...]
+    occupancy: Mapping[Key, tuple[Occupancy, ...]]
+    price_items: Mapping[Key, Mapping[Token, tuple[PriceItem, ...]]]
+    properties: Mapping[Key, tuple[Property, ...]]
+    tags: TagsDict
 
 class Brand(TypedDict, total=False):
     booking: tuple[BookingGroup, ...]
@@ -252,19 +323,19 @@ DefineComponent = tuple[e.Role, _DefineComponent]
 class Oneway(TypedDict, total=False):
     arrival: Required[SimpleNodeIataAirportCode]
     booking_class: Required[Mapping[Key, BookingClass]]
+    check_out_date_offset: CheckOutDateOffset
     departure: Required[SimpleNodeIataAirportCode]
     filter: Mapping[Key, ConditionGroup]
+    neighbour_component_correction: Mapping[Key, NeighbourComponentCorrection]
+    price_items: Mapping[Key, Mapping[Token, tuple[PriceItem, ...]]]
+    properties: Mapping[Key, tuple[Property, ...]]
+    tags: TagsDict
 
 class Flights(TypedDict, total=False):
     oneway: Mapping[Key, Oneway]
 
 class GlobalValue(TypedDict):
     params: Mapping[Key, ParameterSet]
-
-class OccupancyPerson(TypedDict, total=False):
-    count: int
-    max_age: Age
-    min_age: Age
 
 class Product(TypedDict, total=False):
     components: Required[tuple[Component, ...]]
